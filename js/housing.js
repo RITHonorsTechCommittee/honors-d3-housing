f909aa8a31dd8be718f430d62a29bee284755002
regdoug <pierce.reginald@gmail.com>
2013-09-26 19:51:35 -0400

    housing.js ready for first test

---
+++ js/housing.js
@@ -0,0 +1,43 @@
+var housing = {
+	load: function(data,d3svg) {
+		// this href will need to be changed
+		var imghref = "../img/049-"+data.number+".png";
+		var img = d3svg.select(".bgimage");
+		if(img.empty()){
+			d3svg.append("image")
+				.attr("class",".bgimage")
+				.attr("x",0)
+				.attr("y",0)
+				.attr("width",1024)
+				.attr("height",1325)
+				//will we have the right xmlns for this?
+				.attr("xlink:href",imghref);
+		}else{
+			img.attr("xlink:href",imghref);
+		}
+		d3svg.selectAll("circle")
+				.data(data.rooms)
+			.enter()
+				.data(data.rooms)
+				.attr("x",housing.style.x)
+				.attr("y",housing.style.y)
+				.attr("r",housing.style.r)
+				.attr("fill",housing.style.color);
+		//Will that be enough?
+	},
+	
+	style: {
+		x: function(d){ return d.x; },
+		y: function(d){ return d.y; },
+		color: function(d){ 
+			if(0 == d.occupants.length()){
+				return "#00ff00";
+			}else if(d.occupants.length() == d.capacity){
+				return "#ff0000";
+			}else{
+				return "#0000ff";
+			}
+		},
+		r: function(d){ return 5 + Math.sqrt(30*d.capacity); }
+	}
+}
