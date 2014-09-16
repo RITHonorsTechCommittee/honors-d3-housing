eb79f691872ccb9d81dd30f0d6713fa888294b64
regdoug <pierce.reginald@gmail.com>
2014-09-16 14:31:55 -0400

    Merge testing improvements into master
    The master branch, unlike the testing branch, contains the bower
    components because that allows anyone to easily fork the code.
    
    Improvements from testing include piechart occupancy indication
    and interfloor navigation as well as a new JSON spec.

--- js/housing.js
+++ js/housing.js
@@ -9,18 +9,31 @@ var housing = {
     /**
      * Starts the housing selection app
      *
-     * Currently this function simply sets up tooltips for the data.
+     * Currently this function sets up tooltips for the data
+     * and creates a navigation element.
      *
      * @param d3svg An SVG element in which the data will be drawn.
      */
-    init: function(d3svg) {
+    init: function(d3svg,nav,data) {
         // Set up fancy tooltips for the rooms
         housing.tooltip = d3.tip()
             .attr("class","tooltip")
-            .html(function(d) {
-                return "<strong>"+d.number+"</strong>";
-            });
+            .html(housing.style.tooltip);
         d3svg.call(housing.tooltip);
+        // Create navigation
+        if( nav && data && data.length ) {
+            var li = nav.selectAll("li")
+                .data(data);
+            li.exit().remove()
+            li.enter().append("li")
+                .append("a")
+                    .attr("href","#")
+                    .text(function(d){ return "Floor "+d.number; })
+                    .on("click", function(d){ 
+                        housing.load(data,d.number,d3svg); 
+                        return false;
+                    });
+        }
     },
 
     /**
@@ -54,19 +67,35 @@ var housing = {
             if( data[i].number == floor ){
                 // Select all circles in the SVG canvas
                 // and bind them to the rooms on the floor
-                var rooms = d3svg.selectAll("circle")
+                var rooms = d3svg.selectAll(".circle")
                     .data(data[i].rooms)
                 // Remove excess circles if needed
                 rooms.exit().remove();
                 // Add more circles if needed
-                rooms.enter()
-                    .append("circle")
-                    .attr("cx",housing.style.x)
-                    .attr("cy",housing.style.y)
+                
+                var group = rooms.enter().append("g")
+                    .attr("transform",housing.style.transform)
+                    .attr("class","circle");
+                if(housing.tooltip){
+                    group.on("mouseover",housing.tooltip.show)
+                        .on("mouseout",housing.tooltip.hide);
+                }
+                group.append("circle")
                     .attr("r",housing.style.r)
-                    .attr("fill",housing.style.color)
-                    .on("mouseover",housing.tooltip.show)
-                    .on("mouseout",housing.tooltip.hide);
+                    .attr("fill",housing.style.color.empty)
+                    .attr("stroke","black");
+                var arc1 = d3.svg.arc()
+                    .innerRadius(0)
+                    .outerRadius(housing.style.r)
+                    .startAngle(0)
+                    .endAngle(housing.style.endAngle);
+                group.append("path")
+                    .attr("d", arc1)
+                    .attr("fill",housing.style.color.partial);
+                group.append("text")
+                    .text(housing.style.title)
+                    .attr("text-anchor","middle")
+                    .attr("dy",housing.style.titleOffset);
             }
         }
     },
@@ -75,18 +104,37 @@ var housing = {
      * The style namespace contains functions to style d3 elements
      */
     style: {
+        transform: function(d){ return "translate("+d.x+" "+d.y+")"; },
         x: function(d){ return d.x; },
         y: function(d){ return d.y; },
-        color: function(d){ 
-            if(0 == d.occupants.length){
-                return "#00ff00";
-            }else if(d.occupants.length == d.capacity){
-                return "#ff0000";
+        color: function(d){
+            if(typeof d === "undefined") {
+                return housing.style.color;
+            }
+            // find color based on number of occupants
+            if(0 == d.occupants){
+                return housing.style.color.empty;
+            }else if(d.occupants == d.capacity){
+                return housing.style.color.full;
             }else{
-                return "#0000ff";
+                return housing.style.color.partial;
             }
         },
-        r: function(d){ return 4*Math.sqrt(Math.abs(10.5*d.capacity - 1)); }
+        r: function(d){ return 4*Math.sqrt(Math.abs(10.5*d.capacity - 1)); },
+        endAngle: function(d){ return 6.28319 * d.occupants / d.capacity; },
+        tooltip: function(d) {
+            return "<strong>"+d.number+"</strong>";
+        },
+        title: function(d) {
+            return d.number;
+        },
+        titleOffset: function(d) {
+            return housing.style.r(d) + 10;
+        }
     }
 };
+// Allow for statements of the form housing.style.color.empty
+housing.style.color.empty = "#00ff00";
+housing.style.color.partial = "#0000ff";
+housing.style.color.full = "#ff0000";
 
